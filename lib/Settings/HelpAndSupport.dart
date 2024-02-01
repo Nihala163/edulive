6e6bdf3d3745492d8c75a36343f8fb3b48bf3e65
pranave2003 <pranav2003a10@gmail.com>
2024-02-01 14:42:51 +0530

    Fifth commit

--- lib/Settings/HelpAndSupport.dart
+++ lib/Settings/HelpAndSupport.dart
@@ -24,163 +24,161 @@ class _HelpAndSupportState extends State<HelpAndSupport> {
               style: GoogleFonts.poppins(
                   fontWeight: FontWeight.w600, fontSize: 25.sp)),
           centerTitle: true),
-      body: SingleChildScrollView(
-        child: Column(children: [
-          Padding(
-            padding: EdgeInsets.only(top: 30.h),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                    radius: 50.r, backgroundImage: AssetImage("assets/cc.png")),
-              ],
-            ),
+      body: ListView(children: [
+        Padding(
+          padding: EdgeInsets.only(top: 30.h),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                  radius: 50.r, backgroundImage: AssetImage("assets/helpsupport.png")),
+            ],
           ),
-          Padding(
-            padding: EdgeInsets.only(top: 30.h),
-            child: Container(
-              width: 330.w,
-              height: 500.h,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(width: 2)),
-              child: Column(children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 20.h, left: 20),
-                  child: Row(
-                    children: [
-                      Text("Your Name",
-                          style: GoogleFonts.poppins(
-                              fontSize: 15, color: Colors.grey)),
-                    ],
-                  ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(20),
+          child: Container(
+            width: 300.w,
+            height: 500.h,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(width: 2)),
+            child: Column(children: [
+              Padding(
+                padding: EdgeInsets.only(top: 20.h, left: 20),
+                child: Row(
+                  children: [
+                    Text("Your Name",
+                        style: GoogleFonts.poppins(
+                            fontSize: 15, color: Colors.grey)),
+                  ],
                 ),
-                SizedBox(
-                  width: 300.w,
-                  height: 50.h,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: TextFormField(
-                        decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.grey)),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10.r)))),
-                  ),
+              ),
+              SizedBox(
+                width: 300.w,
+                height: 50.h,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey)),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.r)))),
                 ),
-                //
-                //
-                Padding(
-                  padding: EdgeInsets.only(top: 20.h, left: 20),
-                  child: Row(
-                    children: [
-                      Text("Email",
-                          style: GoogleFonts.poppins(
-                              fontSize: 15, color: Colors.grey)),
-                    ],
-                  ),
+              ),
+              //
+              //
+              Padding(
+                padding: EdgeInsets.only(top: 20.h, left: 20),
+                child: Row(
+                  children: [
+                    Text("Email",
+                        style: GoogleFonts.poppins(
+                            fontSize: 15, color: Colors.grey)),
+                  ],
                 ),
-                SizedBox(
-                  width: 300.w,
-                  height: 50.h,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: TextFormField(
-                        decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.grey)),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10.r)))),
-                  ),
+              ),
+              SizedBox(
+                width: 300.w,
+                height: 50.h,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey)),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.r)))),
                 ),
-                //
-                //
-                Padding(
-                  padding: EdgeInsets.only(top: 20.h, left: 20),
-                  child: Row(
-                    children: [
-                      Text("Phone number",
-                          style: GoogleFonts.poppins(
-                              fontSize: 15, color: Colors.grey)),
-                    ],
-                  ),
+              ),
+              //
+              //
+              Padding(
+                padding: EdgeInsets.only(top: 20.h, left: 20),
+                child: Row(
+                  children: [
+                    Text("Phone number",
+                        style: GoogleFonts.poppins(
+                            fontSize: 15, color: Colors.grey)),
+                  ],
                 ),
-                SizedBox(
-                  width: 300.w,
-                  height: 50.h,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: TextFormField(
-                        decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.grey)),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10.r)))),
-                  ),
+              ),
+              SizedBox(
+                width: 300.w,
+                height: 50.h,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey)),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.r)))),
                 ),
-                //
-                //
-                Padding(
-                  padding: EdgeInsets.only(top: 20.h, left: 20),
-                  child: Row(
-                    children: [
-                      Text("Problem description",
-                          style: GoogleFonts.poppins(
-                              fontSize: 15, color: Colors.grey)),
-                    ],
-                  ),
+              ),
+              //
+              //
+              Padding(
+                padding: EdgeInsets.only(top: 20.h, left: 20),
+                child: Row(
+                  children: [
+                    Text("Problem description",
+                        style: GoogleFonts.poppins(
+                            fontSize: 15, color: Colors.grey)),
+                  ],
                 ),
-                SizedBox(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: TextFormField(
-                        maxLines: 5,
-                        decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.grey)),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10.r)))),
-                  ),
+              ),
+              SizedBox(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: TextFormField(
+                      maxLines: 5,
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey)),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.r)))),
                 ),
-              ]),
-            ),
+              ),
+            ]),
           ),
-          //
+        ),
+        //
 
-          Padding(
-            padding: EdgeInsets.only(top: 20.h),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                    height: 50.h,
-                    width: 180.w,
-                    decoration: BoxDecoration(
-                        color: Colors.indigo.shade900,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.black.withOpacity(0.3),
-                              blurRadius: 5.0,
-                              offset: const Offset(0.0, 3.0)),
-                        ]),
-                    child: Center(
-                        child: Text(
-                      "Send",
-                      style: GoogleFonts.poppins(
-                          fontSize: 15.sp,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white),
-                    )),
-                  ),
-                )
-              ],
-            ),
+        Padding(
+          padding: EdgeInsets.only(top: 20.h),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  height: 50.h,
+                  width: 180.w,
+                  decoration: BoxDecoration(
+                      color: Colors.indigo.shade900,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withOpacity(0.3),
+                            blurRadius: 5.0,
+                            offset: const Offset(0.0, 3.0)),
+                      ]),
+                  child: Center(
+                      child: Text(
+                    "Send",
+                    style: GoogleFonts.poppins(
+                        fontSize: 15.sp,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white),
+                  )),
+                ),
+              )
+            ],
           ),
-        ]),
-      ),
+        ),
+      ]),
     );
   }
 }
